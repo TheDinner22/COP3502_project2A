371fbce27d232160f1ce7b9b4fc7f72b686d95c2
joe goodman <jgood1133@gmail.com>
2023-02-15 18:59:48 -0500

    added required methods
    fixed comments

--- rle_program.py
+++ rle_program.py
@@ -1,3 +1,57 @@
+# Joe Goodman
+# Project two part A
+# 02/15/23
+
+# reqs:
+# standalone menu
+# menu option one, two and six
+# 
+#
+#
+#
+
+# Translates data (RLE or raw) a hexadecimal string (without delimiters). This method can also aid debugging. 
+# Ex: to_hex_string([3, 15, 6, 4]) yields string "3f64". 
+def to_hex_string(data):
+    pass
+
+# Returns number of runs of data in an image data set; double this result for length of encoded (RLE) list. 
+# Ex: count_runs([15, 15, 15, 4, 4, 4, 4, 4, 4]) yields integer 2. 
+def count_runs(flat_data):
+    pass
+
+# Returns encoding (in RLE) of the raw data passed in; used to generate RLE representation of a data. 
+# Ex: encode_rle([15, 15, 15, 4, 4, 4, 4, 4, 4]) yields list [3, 15, 6, 4]. 
+def encode_rle(flat_data):
+    pass
+
+# Returns decompressed size RLE data; used to generate flat data from RLE encoding. (Counterpart to #2) 
+# Ex: get_decoded_length([3, 15, 6, 4]) yields integer 9. 
+def get_decoded_length(rle_data):
+    pass
+
+# Returns the decoded data set from RLE encoded data. This decompresses RLE data for use. (Inverse of #3) 
+# Ex: decode_rle([3, 15, 6, 4]) yields list [15, 15, 15, 4, 4, 4, 4, 4, 4, 4]. 
+def decode_rle(rle_data):
+    pass
+
+# Translates a string in hexadecimal format into byte data (can be raw or RLE). (Inverse of #1) 
+# Ex: string_to_data ("3f64") yields list [3, 15, 6, 4]. 
+def string_to_data(data_string):
+    pass
+
+# Translates  RLE data into  a human-readable representation.  For  each  run,  in  order,  it should  display  the  run 
+# length in decimal (1-2 digits); the run value in hexadecimal (1 digit); and a delimiter, ‘:’, between runs. (See 
+# examples in standalone section.) 
+# Ex: to_rle_string([15, 15, 6, 4]) yields string "15f:64". 
+def to_rle_string(rle_data):
+    pass
+
+# Translates a string in human-readable RLE format (with delimiters) into RLE byte data. (Inverse of #7) 
+# Ex: string_to_rle("15f:64") yields list [15, 15, 6, 4].
+def string_to_rle(rle_string):
+    pass
+
 def main():
     pass
 
