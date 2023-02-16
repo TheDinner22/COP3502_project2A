b4ed5639b17574fe85af3f16ddcd978bed0160ce
joe goodman <jgood1133@gmail.com>
2023-02-15 19:15:29 -0500

    to_hex_string impl

--- rle_program.py
+++ rle_program.py
@@ -5,15 +5,20 @@
 # reqs:
 # standalone menu
 # menu option one, two and six
-# 
-#
-#
-#
 
 # Translates data (RLE or raw) a hexadecimal string (without delimiters). This method can also aid debugging. 
 # Ex: to_hex_string([3, 15, 6, 4]) yields string "3f64". 
 def to_hex_string(data):
-    pass
+    # TODO is there a difference between RLE or raw?
+    # TODO can I assume that every item in data is <=15?
+    hex = ""
+
+    # map over every item, converting the decimal to hex
+    for item in data:
+        hex += decimal_to_hex(item)
+
+    # example wants lowercase so that is what we return
+    return hex.lower()
 
 # Returns number of runs of data in an image data set; double this result for length of encoded (RLE) list. 
 # Ex: count_runs([15, 15, 15, 4, 4, 4, 4, 4, 4]) yields integer 2. 
@@ -52,8 +57,40 @@ def to_rle_string(rle_data):
 def string_to_rle(rle_string):
     pass
 
+# code from lab4
+#
+# convert a decimal number to hex
+# using acsii MAGIC!
+#
+# TYPE:
+# - number is an int (positive)
+# - returns a string (hex repr. of number)
+def decimal_to_hex(number):
+    hex = ""
+
+    while number != 0:
+        remainder = number % 16
+
+        if remainder < 10:
+            # 48 just so happens to be the number we have to add
+            # to get to the ascii representation of a number
+            # chr(48) is '0', etc
+            hex += chr(remainder + 48)
+        else:
+            # I have no idea why this works with 55 instead of 54,
+            # but it does
+            hex += chr(remainder + 55)
+
+        number //= 16
+
+    # reverse the order and return
+    return hex[::-1]
+
 def main():
     pass
 
+def tests():
+    assert to_hex_string([3, 15, 6, 4]) == "3f64"
+
 if __name__ == "__main__":
-    main()
+    tests()
