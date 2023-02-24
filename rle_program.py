0e52a318e2f52b79cc2f24e23d444fe55deb6c04
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-02-24 18:30:10 -0500

    re-impl of count_runs

--- rle_program.py
+++ rle_program.py
@@ -46,17 +46,38 @@ def to_hex_string(data):
 # hex encoded rle delegates 1 char to the length and 1 char to the encoded value
 # if flat_data were 10000 1's, the number of runs would not be 1!
 def count_runs(flat_data):
-    # iterate over each pixel and compare its color to the previous pixel's
-    # color
-    # if they are !=, add one to the counter
-    prev = None
-    counter = 0
-    for pixel in flat_data:
-        if pixel != prev:
-            counter += 1 
-        prev = pixel
+    if len(flat_data) == 0:
+        return []
 
-    return counter
+    # first run is created with the first pixel's value
+    current_run = {"length": 1, "value": flat_data[0]}
+    run_counter = 0
+
+    for pixel in flat_data[1:]:
+        # if this pixel is part of the current run OR this is the first iteration
+        if pixel == current_run["value"]:
+            # is the run full?
+            if current_run["length"] == 15:
+                # if so, increment run counter
+                run_counter += 1
+                # initilize new run with pixel
+                current_run = {"length": 1, "value": pixel}
+            else:
+                # if not, then we can just
+                # increase the length of the run
+                current_run["length"] += 1
+        # pixel marks the end of the current run
+        else:
+            # increment run counter
+            run_counter += 1
+            # initilize new run with pixel
+            current_run = {"length": 1, "value": pixel}
+
+    # the last run is not counted so I increment the run_counter here
+    # to compensate
+    run_counter += 1
+
+    return run_counter 
 
 # Returns encoding (in RLE) of the raw data passed in; used to generate RLE representation of a data. 
 # Ex: encode_rle([15, 15, 15, 4, 4, 4, 4, 4, 4]) yields list [3, 15, 6, 4]. 
