b0e4107db7412751c108d6bee4e22e145ad74aff
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-02-24 18:42:14 -0500

    re-impl encode rle method

--- part_2_methods/encode_rle.py
+++ part_2_methods/encode_rle.py
@@ -1,21 +1,30 @@
 # Returns encoding (in RLE) of the raw data passed in; used to generate RLE representation of a data. 
 # Ex: encode_rle([15, 15, 15, 4, 4, 4, 4, 4, 4]) yields list [3, 15, 6, 4]. 
+#
+# again I need to beware the fact that runs have a max length of 15
 def encode_rle(flat_data):
     # if flat_data is empty, return an empty list
     if len(flat_data) == 0:
         return []
 
     rle_data = []
-    current_run = {"value": flat_data[0], "length": 0 }
-    for pixel in flat_data:
-        # is this pixel part of the current run? (or is this the first iteration)
-        if pixel == current_run["value"] or current_run["value"] == None:
-            # increment the length of this run
-            current_run["length"] += 1
+    current_run = {"value": flat_data[0], "length": 1 }
+    for pixel in flat_data[1:]:
+        # is this pixel part of the current run?
+        if pixel == current_run["value"]:
+            # is the current run full?
+            if current_run["length"] == 15:
+                # if it is, add the current run to rle_data
+                rle_data.extend([current_run["length"], current_run["value"]])
+                # and reset current_run with the current pixel
+                current_run = {"value": pixel, "length": 1 }
+            else:
+                # if its not, we can just increment its length
+                current_run["length"] += 1
         else:
             # if it's not, add the current run to rle_data
             rle_data.extend([current_run["length"], current_run["value"]])
-            # and reset current_run
+            # and reset current_run with the current pixel
             current_run = {"value": pixel, "length": 1 }
 
     # the for loop has not added the last run to the rle_data
@@ -23,3 +32,4 @@ def encode_rle(flat_data):
     rle_data.extend([current_run["length"], current_run["value"]])
 
     return rle_data
+
--- rle_program.py
+++ rle_program.py
@@ -81,22 +81,31 @@ def count_runs(flat_data):
 
 # Returns encoding (in RLE) of the raw data passed in; used to generate RLE representation of a data. 
 # Ex: encode_rle([15, 15, 15, 4, 4, 4, 4, 4, 4]) yields list [3, 15, 6, 4]. 
+#
+# again I need to beware the fact that runs have a max length of 15
 def encode_rle(flat_data):
     # if flat_data is empty, return an empty list
     if len(flat_data) == 0:
         return []
 
     rle_data = []
-    current_run = {"value": flat_data[0], "length": 0 }
-    for pixel in flat_data:
-        # is this pixel part of the current run? (or is this the first iteration)
-        if pixel == current_run["value"] or current_run["value"] == None:
-            # increment the length of this run
-            current_run["length"] += 1
+    current_run = {"value": flat_data[0], "length": 1 }
+    for pixel in flat_data[1:]:
+        # is this pixel part of the current run?
+        if pixel == current_run["value"]:
+            # is the current run full?
+            if current_run["length"] == 15:
+                # if it is, add the current run to rle_data
+                rle_data.extend([current_run["length"], current_run["value"]])
+                # and reset current_run with the current pixel
+                current_run = {"value": pixel, "length": 1 }
+            else:
+                # if its not, we can just increment its length
+                current_run["length"] += 1
         else:
             # if it's not, add the current run to rle_data
             rle_data.extend([current_run["length"], current_run["value"]])
-            # and reset current_run
+            # and reset current_run with the current pixel
             current_run = {"value": pixel, "length": 1 }
 
     # the for loop has not added the last run to the rle_data
@@ -326,11 +335,10 @@ def tests():
     assert count_runs([1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]) == 25
     assert count_runs([4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8,7 ]) == 6
 
-    print(encode_rle([4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8,7]))
     assert encode_rle([4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8,7]) == [2,4,15,1,15,1,5,1,1,8,1,7]
     assert encode_rle([1,2,3,4,1,2,3,4]) == [1,1,1,2,1,3,1,4,1,1,1,2,1,3,1,4]
     assert encode_rle([4,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]) == [2,4,15,1,15,1,5,1]
-    assert encode_rle([4,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]) == [1,4,1,515,1,15,1,5,1]
+    assert encode_rle([4,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]) == [1,4,1,5,15,1,15,1,5,1]
 
 
 if __name__ == "__main__":
