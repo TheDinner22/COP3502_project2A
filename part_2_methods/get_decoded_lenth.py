8b6748c6d1d628f602a816a40d9bf4296bb7da7b
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-02-24 18:56:38 -0500

    did not need these

--- part_2_methods/get_decoded_lenth.py
+++
@@ -1,5 +0,0 @@
-# Returns decompressed size RLE data; used to generate flat data from RLE encoding. (Counterpart to #2) 
-# Ex: get_decoded_length([3, 15, 6, 4]) yields integer 9. 
-def get_decoded_length(rle_data):
-    # sum every other element in rle data
-    return sum(rle_data[::2])
